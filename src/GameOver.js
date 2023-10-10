a1ed0d9cba43a5c1d9a96973589bfcee6acd6263
Ismael Ignacio Miguel Torres <ignacio25.it@gmail.com>
2023-10-10 20:41:23 -0300

    Pantalla de Game Over finalizada (con imágenes de Albornoz)

--- src/GameOver.js
+++ src/GameOver.js
@@ -11,18 +11,18 @@ class GameOver extends Phaser.Scene {
     }
 
     preload(){
-        this.load.image('gameover-screen', '/public/img/gameover-screen.png')
-        this.load.image('restart-button', '/public/img/restart-button.png')
+        this.load.image('gameover-screen', '/public/img/gameover-screen.jpeg')
+        this.load.image('restart-button', '/public/img/restart-button.jpeg')
     }
 
     create(){
 
-        this.add.image(400, 200, 'gameover-screen');
-        this.startButton = this.add.image(400, 450, 'restart-button').setInteractive();
+        this.add.image(400, 300, 'gameover-screen');
+        this.startButton = this.add.image(400, 450, 'restart-button').setScale(0.2).setInteractive();
         this.startButton.on('pointerdown', () => {
             this.scene.start('Nivel1');
         });
-        this.puntuacionFinalText = this.add.text(160, 350, "Puntaje final: "+ this.puntajeFinal, {
+        this.puntuacionFinalText = this.add.text(160, 300, "Puntaje final: "+ this.puntajeFinal, {
             fontSize: "36px",
             fill: "#fff",
             fontFamily: "dogicapixelbold"
